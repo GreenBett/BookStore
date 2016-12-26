3cdade5a2d6bb6214710f3fadee644acd9e2bb24
GreenBett <green95551@protonmail.com>
2016-12-26 15:21:42 -0300

    Create controllers and Implement List, Create and Detail for Publishers

---
+++ src/BookStore/Controllers/PublishersController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookStore.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore.Controllers
+{
+    public class PublishersController : Controller
+    {
+       private  StoreContext _context;
+        public PublishersController(StoreContext context)
+        {
+            _context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_context.Publishers.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Publisher publisher)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Publishers.Add(publisher);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(publisher);
+        }
+
+        [ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            Publisher publisher = _context.Publishers.First(o => o.PublisherId == id);
+            if (publisher != null)
+            {
+                return View(publisher);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Publisher publisher = _context.Publishers.First(o => o.PublisherId == id);
+            _context.Publishers.Remove(publisher);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details (int? id)
+        {
+            Publisher publisher = _context.Publishers.First(o => o.PublisherId == id);
+            if (publisher != null)
+            {
+                return View(publisher);
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
